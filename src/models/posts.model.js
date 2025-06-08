e7827aae38fb469003aafb7626141dff1a9da542
Pablopserrada <pablopserrada@gmail.com>
2025-06-08 02:28:36 +0200

    estableccemos las funciones getAll para posts y autores

---
+++ src/models/posts.model.js
@@ -0,0 +1,10 @@
+const db = require('../config/db')
+
+const selectAll = async () => {
+    const [result] = await db.query('SELECT * FROM posts')
+    return result;
+}
+
+module.exports = {
+    selectAll
+}
